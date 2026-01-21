ca80f02b10680b1b68d6b1742bc794521b03f408
Björn Ricks <bjoern.ricks@greenbone.net>
2026-01-21 14:42:13 +0100

    Add: Introduce an own error class for error responses
    Make creating error responses easy.

--- src/service/middleware.rs
+++ src/service/middleware.rs
@@ -4,31 +4,26 @@
 
 use axum::{
     extract::{Request, State},
-    http::StatusCode,
     middleware::Next,
     response::Response,
 };
 
-use crate::{jwt::validate_token, service::app::AppState};
+use crate::{jwt::validate_token, service::app::AppState, service::error::Error};
 
 pub async fn authorization_middleware(
     State(state): State<AppState>,
     request: Request,
     next: Next,
-) -> Result<Response, (StatusCode, &'static str)> {
+) -> Result<Response, Error> {
     let headers = request.headers();
-    let auth_header = headers
-        .get("Authorization")
-        .ok_or((StatusCode::UNAUTHORIZED, "Unauthorized"))?;
-    let auth_str = auth_header
-        .to_str()
-        .map_err(|_| (StatusCode::UNAUTHORIZED, "Unauthorized"))?;
+    let auth_header = headers.get("Authorization").ok_or(Error::Unauthorized)?;
+    let auth_str = auth_header.to_str().map_err(|_| Error::Unauthorized)?;
     let token = auth_str
         .strip_prefix("Bearer ")
-        .ok_or((StatusCode::UNAUTHORIZED, "Unauthorized"))?;
+        .ok_or(Error::Unauthorized)?;
 
     match validate_token(&state.jwt_secret, token) {
         Ok(_) => Ok(next.run(request).await),
-        Err(_) => Err((StatusCode::UNAUTHORIZED, "Unauthorized")),
+        Err(_) => Err(Error::Unauthorized),
     }
 }
