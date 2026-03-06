e0684852bd29e24061bb98f758344c291c34ab19
Björn Ricks <bjoern.ricks@greenbone.net>
2026-03-06 14:27:52 +0100

    Add feed key validation when uploading a key
    Implement the actual feed key validation when a feed key is uploaded.

--- src/service/api/key.rs
+++ src/service/api/key.rs
@@ -2,6 +2,8 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+use std::{io, path::Path};
+
 use axum::{
     Router,
     body::Body,
@@ -10,16 +12,19 @@ use axum::{
     response::IntoResponse,
     routing::{get, put},
 };
-use tokio::io::{AsyncWriteExt, BufWriter};
-use tokio_stream::StreamExt;
+use futures::TryStreamExt;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter};
 use tokio_util::io::{ReaderStream, StreamReader};
 use utoipa::{OpenApi, ToSchema};
 
-use crate::service::{
-    app::{AppRouter, AppState},
-    error::Error,
-    middleware::authorization_middleware,
-    response::JsonResponse,
+use crate::{
+    service::{
+        app::{AppRouter, AppState},
+        error::Error,
+        middleware::authorization_middleware,
+        response::JsonResponse,
+    },
+    validation::{Base64FeedKeyValidator, FeedKeyValidator},
 };
 
 const KEY_TAG: &str = "Key";
@@ -33,7 +38,7 @@ const DEFAULT_UPLOAD_LIMIT: usize = 2 * 1024 * 1024; // 2 MB
 )]
 pub struct KeyApi;
 
-/// Download the current feed key as a PEM file.
+/// Download the current feed key file.
 #[utoipa::path(
   get,
   path = "",
@@ -42,7 +47,7 @@ pub struct KeyApi;
         status = 200,
         description = "Key downloaded successfully",
         body = String,
-        content_type = "application/x-pem-file"
+        content_type = "application/octet-stream",
     ),
     (
         status = 401,
@@ -86,7 +91,7 @@ async fn download_key(State(state): State<AppState>) -> Result<impl IntoResponse
     let stream = ReaderStream::new(file);
     let body = Body::from_stream(stream);
     let headers = [
-        (header::CONTENT_TYPE, "application/x-pem-file".to_string()),
+        (header::CONTENT_TYPE, "application/octet-stream".to_string()),
         (header::CONTENT_DISPOSITION, content_disposition),
         (header::CONTENT_LENGTH, "".to_string()),
         (header::TRANSFER_ENCODING, "chunked".to_string()),
@@ -94,7 +99,67 @@ async fn download_key(State(state): State<AppState>) -> Result<impl IntoResponse
     Ok((headers, body))
 }
 
-/// Upload a new feed key as a PEM file.
+async fn validate_and_read_feed_key<R>(reader: &mut BufReader<R>) -> Result<Vec<String>, Error>
+where
+    R: tokio::io::AsyncRead + Unpin,
+{
+    let mut validator = Base64FeedKeyValidator::new();
+    let mut lines: Vec<String> = Vec::new();
+    loop {
+        let mut line = String::new();
+        let len = reader.read_line(&mut line).await.map_err(|e| {
+            tracing::error!("Failed to read uploaded file: {}", e);
+            Error::InternalServerError("Key upload failed. Could not read file.".to_string())
+        })?;
+        if len == 0 {
+            validator.done().map_err(|e| {
+                tracing::error!("Key validation failed: {}", e);
+                Error::BadRequest(format!("Key upload failed. Failed to validate key. {}", e))
+            })?;
+            break;
+        }
+        validator.push(&line).map_err(|e| {
+            tracing::error!("Key validation failed on '{}': {}", line.trim(), e);
+            Error::BadRequest(format!("Key upload failed. Failed to validate key. {}", e))
+        })?;
+        lines.push(line);
+    }
+
+    Ok(lines)
+}
+
+async fn write_feed_key(feed_key_path: &Path, lines: Vec<String>) -> Result<(), Error> {
+    let file = match tokio::fs::File::create(feed_key_path).await {
+        Ok(file) => file,
+        Err(err) => {
+            tracing::error!(
+                "Failed to create key file {}: {}",
+                feed_key_path.display(),
+                err
+            );
+            return Err(Error::InternalServerError(
+                "Key upload failed. File error.".to_string(),
+            ));
+        }
+    };
+    let mut writer = BufWriter::new(file);
+    for line in lines {
+        writer.write_all(line.as_bytes()).await.map_err(|e| {
+            tracing::error!("Failed to write key data: {}", e);
+            Error::InternalServerError("Key upload failed. Could not write to file.".to_string())
+        })?;
+    }
+
+    writer.flush().await.map_err(|e| {
+        tracing::error!("Failed to flush key file writer: {}", e);
+        Error::InternalServerError("Key upload failed. Could not write to file.".to_string())
+    })?;
+
+    tracing::info!("Successfully wrote key file {}", feed_key_path.display());
+    Ok(())
+}
+
+/// Upload a new feed key file.
 #[utoipa::path(
   put,
   path = "",
@@ -105,6 +170,13 @@ async fn download_key(State(state): State<AppState>) -> Result<impl IntoResponse
         body = JsonResponse,
         content_type= "application/json",
         example = json!({"status": "success", "message": "Key uploaded successfully"})
+    ),
+    (
+        status = 400,
+        description = "Bad Request. Failed to validate key.",
+        body = JsonResponse,
+        content_type= "application/json",
+        example = json!({"status": "error", "message": "Bad request: Key upload failed. Failed to validate key. Invalid Key data"})
     ),
     (
         status = 401,
@@ -122,56 +194,38 @@ async fn download_key(State(state): State<AppState>) -> Result<impl IntoResponse
     ),
     (
         status = 500,
-        description = "Key upload failed. Stream error.",
+        description = "Key upload failed. Could not read file.",
         body = JsonResponse,
         content_type= "application/json",
-        example = json!({"status": "error", "message": "Key upload failed. Stream error."})
+        example = json!({"status": "error", "message": "Key upload failed. Could not read file."})
+    ),
+    (
+        status = 500,
+        description = "Key upload failed. Could not write to file.",
+        body = JsonResponse,
+        content_type= "application/json",
+        example = json!({"status": "error", "message": "Key upload failed. Could not write to file."})
     ),
   ),
   tag = KEY_TAG,
-  request_body(content(("application/x-pem-file"),("application/octet-stream")), description = "The key file to upload"),
+  request_body(content(("application/octet-stream")), description = "The key file to upload"),
   security(("jwt_auth" = []))
 )]
 async fn upload_key(
     State(state): State<AppState>,
     request: Request,
 ) -> Result<JsonResponse, Error> {
-    let file = match tokio::fs::File::create(&state.feed_key_path).await {
-        Ok(file) => file,
-        Err(err) => {
-            tracing::error!(
-                "Failed to create key file {}: {}",
-                state.feed_key_path.display(),
-                err
-            );
-            return Err(Error::InternalServerError(
-                "Key upload failed. File error.".to_string(),
-            ));
-        }
-    };
-    let stream = request.into_body().into_data_stream();
-    let stream = stream.map(|result| result.map_err(std::io::Error::other));
-    let mut reader = StreamReader::new(stream);
-    let mut writer = BufWriter::new(file);
-    match tokio::io::copy(&mut reader, &mut writer).await {
-        Ok(_) => {
-            tracing::info!(
-                "Successfully wrote key file {}",
-                state.feed_key_path.display()
-            );
-            Ok(JsonResponse::from_success("Key uploaded successfully"))
-        }
-        Err(err) => {
-            tracing::error!(
-                "Failed to write key file {}: {}",
-                state.feed_key_path.display(),
-                err
-            );
-            Err(Error::InternalServerError(
-                "Key upload failed. Stream error.".to_string(),
-            ))
-        }
-    }
+    let stream = request
+        .into_body()
+        .into_data_stream()
+        .map_err(io::Error::other);
+    let stream_reader = StreamReader::new(stream);
+    let mut reader = BufReader::new(stream_reader);
+
+    let lines = validate_and_read_feed_key(&mut reader).await?;
+    write_feed_key(&state.feed_key_path, lines).await?;
+
+    Ok(JsonResponse::from_success("Key uploaded successfully"))
 }
 
 // only for utoipa documentation
@@ -211,10 +265,10 @@ struct UploadedForm {
     ),
     (
         status = 400,
-        description = "Key upload failed. Could not read file field.",
+        description = "Bad Request. Failed to validate key.",
         body = JsonResponse,
         content_type= "application/json",
-        example = json!({"status": "error", "message": "Key upload failed. Could not read file field."})
+        example = json!({"status": "error", "message": "Bad request: Key upload failed. Failed to validate key. Invalid Key data"})
     ),
     (
         status = 401,
@@ -262,7 +316,7 @@ async fn upload_key_multipart(
             ));
         }
     };
-    let mut field = match field {
+    let field = match field {
         Some(f) => f,
         None => {
             tracing::error!("No file provided in multipart upload");
@@ -280,47 +334,12 @@ async fn upload_key_multipart(
             ));
         }
     };
-    let file = match tokio::fs::File::create(&state.feed_key_path).await {
-        Ok(file) => file,
-        Err(err) => {
-            tracing::error!(
-                "Failed to create key file {}: {}",
-                state.feed_key_path.display(),
-                err
-            );
-            return Err(Error::InternalServerError(
-                "Key upload failed. File error.".to_string(),
-            ));
-        }
-    };
+    let stream = StreamReader::new(field.map_err(io::Error::other));
+    let mut reader = BufReader::new(stream);
 
-    let mut writer = BufWriter::new(file);
-    while let Some(chunk) = field.chunk().await.map_err(|_| {
-        Error::BadRequest("Key upload failed. Could not read file field".to_string())
-    })? {
-        if let Err(e) = writer.write(&chunk).await {
-            tracing::error!(
-                "Failed to write key file {}: {}",
-                state.feed_key_path.display(),
-                e
-            );
-            return Err(Error::InternalServerError(
-                "Key upload failed. Stream error.".to_string(),
-            ));
-        }
-    }
-    writer.flush().await.map_err(|err| {
-        tracing::error!(
-            "Failed to flush key file {}: {}",
-            state.feed_key_path.display(),
-            err
-        );
-        Error::InternalServerError("Key upload failed. Could not write to file.".to_string())
-    })?;
-    tracing::info!(
-        "Successfully wrote key file {}",
-        state.feed_key_path.display()
-    );
+    let lines = validate_and_read_feed_key(&mut reader).await?;
+    write_feed_key(&state.feed_key_path, lines).await?;
+
     Ok(JsonResponse::from_success("Key uploaded successfully"))
 }
 
