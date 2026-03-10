eeabe48fc31c080003111a2b79d0f0623182a710
Björn Ricks <bjoern.ricks@greenbone.net>
2026-03-10 11:07:06 +0100

    Change: Improve tracing logs for authorization handling
    Allow to debug Authorization failures more easily.

--- src/service/middleware.rs
+++ src/service/middleware.rs
@@ -4,6 +4,7 @@
 
 use axum::{
     extract::{Request, State},
+    http,
     middleware::Next,
     response::Response,
 };
@@ -17,14 +18,30 @@ pub async fn authorization_middleware(
     next: Next,
 ) -> Result<Response, Error> {
     let headers = request.headers();
-    let auth_header = headers.get("Authorization").ok_or(Error::Unauthorized)?;
-    let auth_str = auth_header.to_str().map_err(|_| Error::Unauthorized)?;
-    let token = auth_str
-        .strip_prefix("Bearer ")
-        .ok_or(Error::Unauthorized)?;
+    let auth_header = match headers.get(http::header::AUTHORIZATION) {
+        Some(header) => header,
+        None => {
+            tracing::debug!("Missing Authorization header {:?}", request);
+            return Err(Error::Unauthorized);
+        }
+    };
+    let auth_str = auth_header.to_str().map_err(|e| {
+        tracing::debug!("Unable to convert Authorization header into string: {}", e);
+        Error::Unauthorized
+    })?;
+    let token = match auth_str.strip_prefix("Bearer ") {
+        Some(token) => token,
+        None => {
+            tracing::debug!("Could not extract JWT token from Authorization header");
+            return Err(Error::Unauthorized);
+        }
+    };
 
     match validate_token(&state.jwt_decode_secret, token) {
         Ok(_) => Ok(next.run(request).await),
-        Err(_) => Err(Error::Unauthorized),
+        Err(e) => {
+            tracing::debug!("Invalid JWT token {}", e);
+            Err(Error::Unauthorized)
+        }
     }
 }
